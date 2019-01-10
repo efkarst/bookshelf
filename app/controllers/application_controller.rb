fbc4943645b8dc113d8aa73dd3d23e95eae24b55
Elizabeth <efkarst@outlook.com>
2019-01-09 18:45:55 -0800

    Add header to layout for core nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  # helper_methods :logged_in?
-  # helper_method :current_user
+  helper_method :logged_in?
+  helper_method :current_user
 
   def logged_in?
     !!session[:user_id]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def first_name
+    current_user.name.split(" ")[0]
+  end
+
+  def show_svg(path)
+    File.open("app/assets/images/#{path}", "rb") do |file|
+      raw file.read
+    end
+  end
 end
