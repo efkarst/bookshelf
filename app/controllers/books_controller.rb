4c45e3b743945d598c6667c258072ae95b2fd418
Elizabeth <efkarst@outlook.com>
2019-01-13 17:54:38 -0800

    Add ability for user to remove book from collection

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,14 +18,23 @@ class BooksController < ApplicationController
     @books = BookFinder.search_google_books_by_title(params[:search])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.users << current_user
+    @book.save
+
+  end
+
   def create
     @book = Book.where(identifier: params[:book][:identifier]).first_or_create(book_params)
-    binding.pry
-    # current_user.books << @book
-    # current_user.save
-
     redirect_to user_path(current_user)
+  end
 
+  def destroy
+    @book = Book.find(params[:id])
+    UserBook.remove_user_book_association(current_user.id, @book.id)
+    @book.destroy if @book.users.empty?
+    redirect_to user_path(current_user)
   end
 
   private
--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -1,4 +1,8 @@
 class UserBook < ApplicationRecord
   belongs_to :user 
   belongs_to :book
+
+  def self.remove_user_book_association(user_id, book_id)
+    UserBook.where("user_id=#{user_id}").where("book_id=#{book_id}").first.destroy
+  end
 end
