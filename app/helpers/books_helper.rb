e0b63a834b097bed815340f4b7d267cceb0e7329
Elizabeth <efkarst@outlook.com>
2019-01-19 19:31:48 -0800

    fix bug where read label was blocking browse book view

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -38,6 +38,6 @@ module BooksHelper
   end
 
   def current_user_finished_book(book)
-    finished = UserBook.where("user_id = #{current_user.id}").where("book_id=#{book.id}").first.finished_book
+    finished = UserBook.where("user_id = #{current_user.id}").where("book_id=#{book.id}").first.finished_book if current_user_book(book)
   end
 end
