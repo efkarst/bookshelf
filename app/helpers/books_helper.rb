b564d6be7189ea1a814b6f5c16f06a69db323d85
Elizabeth <efkarst@outlook.com>
2019-01-22 12:54:23 +0000

    Add comments to book index view

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -4,15 +4,11 @@ module BooksHelper
   def add_or_remove_button(book)
     if !current_user_book(book)
       render 'books/add_to_collection', {book: book}
+    elsif !book.id
+      render 'new_book_from_search_form', {book: book}
     else
       render 'books/remove_from_collection', {book: book}
     end
-  end
-
-  def book_filters
-    select "filter", "<option value=filter[order_by_title]>Order by Title</option>
-                      <option value=filter[order_by_author]>Order by Author</option>
-                      <option value=filter[top_rated]>Top Rated</option>".html_safe, class: 'button'
   end
 
   # Returns instance of book - do i need this or can i just use method below?
