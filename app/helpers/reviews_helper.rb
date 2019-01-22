97666b25ace11f2d6ffc483990cab2f349fca320
Elizabeth <efkarst@outlook.com>
2019-01-22 14:24:57 +0000

    remove unneeded review helper methods

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -5,16 +5,7 @@ module ReviewsHelper
     UserBook.where("user_id = #{user_id}").where("book_id = #{book_id}").first.id
   end
 
-  def current_user_has_reviewed?(book)
-    # binding.pry
-    res = false
-    book.reviews.each do |review|
-      res = true if review.user == current_user
-    end
-    res
-  end
-
-
+  # Returns current users review of provided book
   def current_users_book_review(book)
     UserBook.where("user_id = #{current_user.id}").where("book_id = #{book.id}").first.review
   end
