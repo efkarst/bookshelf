7ab4b4936b0ab38758ca76e252835e28b090362a
Elizabeth <efkarst@outlook.com>
2019-01-28 10:36:48 -0800

    Update ReviewsHelper to move database query to model

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -7,7 +7,7 @@ module ReviewsHelper
 
   # Returns current users review of provided book
   def current_users_book_review(book)
-    UserBook.where("user_id = #{current_user.id}").where("book_id = #{book.id}").first.review
+    UserBook.find_user_book_activity(current_user.id, book.id).review
   end
 
 end
