f5f1b7c63414ceee7b5c96c7db4f74b28bae80fe
Elizabeth <efkarst@outlook.com>
2019-03-16 12:07:16 -0700

    Remove unused methods in book_shelf model

--- app/models/book_shelf.rb
+++ app/models/book_shelf.rb
@@ -3,8 +3,4 @@ class BookShelf < ApplicationRecord
   belongs_to :book 
   belongs_to :shelf
 
-  ### Updating Associations ###
-  # def self.remove_book_shelf_association(shelf_id, book_id)
-  #   where("shelf_id=#{shelf_id}").where("book_id=#{book_id}").first.destroy
-  # end
 end
